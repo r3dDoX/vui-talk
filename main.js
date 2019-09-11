adcc3c83d1db7785f38f7c65e3535bb9ab247c30
r3dDoX <r3dDoX@users.noreply.github.com>
2019-09-11 13:26:31 +0200

    use two space tabs

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const colorMapping = {
   beige: 'beige',
   schwarz: 'black',
   blau: 'blue',
+  red: 'red',
 };
 const colors = Object.keys(colorMapping);
 const grammar = `#JSGF V1.0; grammar colors; public <color> = ${colors.join(' | ')} ;`;
@@ -26,32 +27,32 @@ const hints = document.querySelector('.hints');
 
 let colorHTML = '';
 colors.forEach(function (color, index) {
-    console.log(color, index);
-    colorHTML += `<span style="background-color:${colorMapping[color]};"> ${color} </span>`;
+  console.log(color, index);
+  colorHTML += `<span style="background-color:${colorMapping[color]};"> ${color} </span>`;
 });
 hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
 
 document.body.onclick = () => {
-    recognition.start();
-    console.log('Ready to receive a color command.');
+  recognition.start();
+  console.log('Ready to receive a color command.');
 };
 
 recognition.onresult = (event) => {
-    const last = event.results.length - 1;
-    const color = event.results[last][0].transcript;
-    diagnostic.textContent = 'Result received: ' + color + '.';
-    bg.style.backgroundColor = colorMapping[color.toLocaleLowerCase()];
-    console.log('Confidence: ' + event.results[0][0].confidence);
+  const last = event.results.length - 1;
+  const color = event.results[last][0].transcript;
+  diagnostic.textContent = 'Result received: ' + color + '.';
+  bg.style.backgroundColor = colorMapping[color.toLocaleLowerCase()];
+  console.log('Confidence: ' + event.results[0][0].confidence);
 };
 
 recognition.onspeechend = () => {
-    recognition.stop();
+  recognition.stop();
 };
 
 recognition.onnomatch = () => {
-    diagnostic.textContent = 'I didnt recognise that color.';
+  diagnostic.textContent = 'I didnt recognise that color.';
 };
 
 recognition.onerror = (event) => {
-    diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
 }
