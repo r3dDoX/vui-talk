0782b338d45183916d9b68211db3733e4afba38e
r3dDoX <r3dDoX@users.noreply.github.com>
2019-09-18 06:53:54 +0200

    remove unused authenticated callback

--- auth.js
+++ auth.js
@@ -3,15 +3,6 @@ import {apiKey, clientId} from './secret.js';
 const scope = 'https://www.googleapis.com/auth/dialogflow';
 
 let GoogleAuth;
-let authenticatedCallback;
-
-export function whenAuthenticated(callback) {
-  if (GoogleAuth && GoogleAuth.currentUser.get().hasGrantedScopes(scope)) {
-    callback();
-  } else {
-    authenticatedCallback = callback;
-  }
-}
 
 document.addEventListener('DOMContentLoaded', function () {
   gapi.load('client:auth2', function () {
@@ -23,8 +14,8 @@ document.addEventListener('DOMContentLoaded', function () {
       .then(function () {
         GoogleAuth = gapi.auth2.getAuthInstance();
         const isAuthorized = GoogleAuth.currentUser.get().hasGrantedScopes(scope);
-        if (isAuthorized && authenticatedCallback) {
-          authenticatedCallback();
+        if (!isAuthorized) {
+          console.error('not authorized');
         }
       })
       .catch(console.error);
